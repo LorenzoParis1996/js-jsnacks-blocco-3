96afcb4c02a5d5df31eee3acc982364f74e09c46
LorezoParis1996 <lorenzoparis86@live.it>
2024-05-14 17:18:32 +0200

    create condizioni per trovare il cocomero

--- array-lista/js/array-lista.js
+++ array-lista/js/array-lista.js
@@ -2,3 +2,9 @@ const frigorifero = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino
 const pesca = frigorifero.push('pesca');
 
 console.log(frigorifero);
+
+if (frigorifero[6].length === 8) {
+    console.log('Trovato! Devo solo preparare il cocktail.');
+} else {
+    console.log('Oh no, devo uscire a comprare il cocomero!');
+}
