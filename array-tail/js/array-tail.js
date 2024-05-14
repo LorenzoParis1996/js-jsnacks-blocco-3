c79998c27535de9980bdaa7dedad45815e3b1888
LorezoParis1996 <lorenzoparis86@live.it>
2024-05-14 18:04:19 +0200

    creato ciclo for per iterare nel array in base al numero scritto dall'utente

--- array-tail/js/array-tail.js
+++ array-tail/js/array-tail.js
@@ -3,3 +3,8 @@ const quantityNumbers = Number.parseInt(prompt('quanti elementi desideri?'));
 let randomNumbers = [];
 
 randomNumbers = [Math.floor(Math.random() * 100) + 1];
+
+for (i = 0; i < quantityNumbers; i++) {
+
+    console.log(quantityNumbers, randomNumbers);
+};
