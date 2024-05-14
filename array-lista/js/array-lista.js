f62e855ffa8fecb90920eb3421c58fe68fea0551
LorezoParis1996 <lorenzoparis86@live.it>
2024-05-14 17:17:34 +0200

    create variabili con controllo log

---
+++ array-lista/js/array-lista.js
@@ -0,0 +1,4 @@
+const frigorifero = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
+const pesca = frigorifero.push('pesca');
+
+console.log(frigorifero);
