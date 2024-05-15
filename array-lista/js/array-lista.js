98d3030b06703b35f992c96ddee6edb89acdd68e
LorezoParis1996 <lorenzoparis86@live.it>
2024-05-15 14:13:14 +0200

    correzione esercizio

--- array-lista/js/array-lista.js
+++ array-lista/js/array-lista.js
@@ -1,9 +1,15 @@
 const frigorifero = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
 const pesca = frigorifero.push('pesca');
 
-console.log(frigorifero);
+let found = false;
 
-if (frigorifero[6].length === 8) {
+for (let index = 0; index < frigorifero.length; index++) {
+    if (frigorifero[index] === 'cocomero') {
+        found = true
+    }
+}
+
+if (found) {
     console.log('Trovato! Devo solo preparare il cocktail.');
 } else {
     console.log('Oh no, devo uscire a comprare il cocomero!');
