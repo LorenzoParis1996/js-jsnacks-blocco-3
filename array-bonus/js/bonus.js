23918482690254ffc9db15b17d8a221bf4ca5a51
LorezoParis1996 <lorenzoparis86@live.it>
2024-05-15 12:33:17 +0200

    corretto esercizio

--- array-bonus/js/bonus.js
+++ array-bonus/js/bonus.js
@@ -2,10 +2,9 @@ let shortArray = [1, 2, 3, 4];
 let longArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 while (shortArray.length < longArray.length) {
-    longArray.push(shortArray());
+    shortArray.push(1);
 
-    shortArray++
-    longArray++
+
 }
 
 console.log(shortArray, longArray);
