e53d5210250a3a292244901ff838c2450f4143e3
LorezoParis1996 <lorenzoparis86@live.it>
2024-05-23 12:14:46 +0200

    da finire

---
+++ snack-objects-2/js/objects-2.js
@@ -0,0 +1,78 @@
+const listaAuto = [
+    {
+        marca: 'fiat',
+        modello: '500',
+        alimentazione: 'metano'
+    },
+    {
+        marca: 'fiat',
+        modello: 'panda',
+        alimentazione: 'benzina'
+    },
+    {
+        marca: 'fiat',
+        modello: 'abarth',
+        alimentazione: 'diesel'
+    },
+    {
+        marca: 'fiat',
+        modello: 'family',
+        alimentazione: 'gpl'
+    },
+    {
+        marca: 'fiat',
+        modello: 'electro',
+        alimentazione: 'elettrico'
+    },
+    {
+        marca: 'ford',
+        modello: 'fiesta',
+        alimentazione: 'diesel'
+    },
+    {
+        marca: 'ford',
+        modello: 'focus',
+        alimentazione: 'benzina'
+    },
+    {
+        marca: 'ford',
+        modello: 'kuga',
+        alimentazione: 'metano'
+    },
+    {
+        marca: 'ford',
+        modello: 'puma',
+        alimentazione: 'gpl'
+    },
+    {
+        marca: 'ford',
+        modello: 'ecosport',
+        alimentazione: 'elettrico'
+    },
+    {
+        marca: 'seat',
+        modello: 'ibiza',
+        alimentazione: 'diesel'
+    },
+    {
+        marca: 'seat',
+        modello: 'leon',
+        alimentazione: 'benzina'
+    },
+    {
+        marca: 'seat',
+        modello: 'arona',
+        alimentazione: 'metano'
+    },
+    {
+        marca: 'seat',
+        modello: 'tarraco',
+        alimentazione: 'gpl'
+    },
+    {
+        marca: 'seat',
+        modello: 'eco',
+        alimentazione: 'elettrico'
+    }
+
+];
