634867da699794f2d07d5ee4ac6ae821fb9448a5
LorezoParis1996 <lorenzoparis86@live.it>
2024-05-24 11:30:49 +0200

    fatta somma pesi

---
+++ snack-zucchine/js/zucchine.js
@@ -0,0 +1,60 @@
+const zucchine = [
+    {
+        varieta: 'uno',
+        peso: 3,
+        lunghezza: 12
+    },
+    {
+        varieta: 'due',
+        peso: 2,
+        lunghezza: 14
+    },
+    {
+        varieta: 'tre',
+        peso: 1,
+        lunghezza: 5
+    },
+    {
+        varieta: 'quattro',
+        peso: 5,
+        lunghezza: 3
+    },
+    {
+        varieta: 'cinque',
+        peso: 3,
+        lunghezza: 10
+    },
+    {
+        varieta: 'sei',
+        peso: 8,
+        lunghezza: 8
+    },
+    {
+        varieta: 'sette',
+        peso: 10,
+        lunghezza: 16
+    },
+    {
+        varieta: 'otto',
+        peso: 7,
+        lunghezza: 13
+    },
+    {
+        varieta: 'nove',
+        peso: 4,
+        lunghezza: 3
+    },
+    {
+        varieta: 'dieci',
+        peso: 9,
+        lunghezza: 7
+    }
+];
+
+let sum = 0;
+
+zucchine.forEach(element => {
+    sum += element.peso
+});
+
+console.log(sum);
